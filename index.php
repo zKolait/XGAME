09fc4834ccc1c7c333c689ba911fa97b5d19f5a6
zKolait <nicolas.chapeau@devinci.fr>
2018-11-06 14:22:56 +0100

    update landing page

---
+++ index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SnowSet</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+
+<body>
+
+    <div class="container">
+        <header>
+
+            <div class="navbar">
+                <div class="logo">
+                    <img src="img/logo.png" alt="Logo SnowSet">
+                    <h1>SNOWSET</h1>
+                </div>
+                <ul>
+                    <li><a href=""><p>NUMERO</p></a></li>
+                    <li><a href=""><p>NUMERO</p></a></li>
+                    <li><a href=""><p>NUMERO</p></a></li>
+                    <li><a href=""><p>NUMERO</p></a></li>
+                </ul>
+            </div>
+
+            <div class="header">
+                <div class="info">
+                    <a>Découvrir</a>
+                </div>
+            </div>
+
+        </header>
+
+        <main>
+
+        </main>
+    </div>
+
+
+
+<?php
+    @include 'includes/footer.php';
+?>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</body>
+
+</html>
