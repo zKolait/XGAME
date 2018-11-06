0be94d75ca989e4c5cc474e65cce33162883667d
zKolait <nicolas.chapeau@devinci.fr>
2018-11-06 10:02:11 +0100

    tree & form update

--- form.php
+++ form.php
@@ -1,29 +1,4 @@
 <!doctype html>
-
-<?php
-require "includes/config.php";
-$reponse = 0;
-if (isset($_POST['NOM']) AND isset($_POST['EMAIL']) AND isset($_POST['OBJET']) AND isset($_POST['MESSAGE'])) {
-
-    if (!empty($_POST['NOM']) AND !empty($_POST['EMAIL']) AND !empty($_POST['OBJET']) AND !empty($_POST['MESSAGE'])) {
-
-        $nom = htmlspecialchars($_POST['NOM']);
-        $email = htmlspecialchars($_POST['EMAIL']);
-        $objet = htmlspecialchars($_POST['OBJET']);
-        $message = htmlspecialchars($_POST['MESSAGE']);
-
-        $InsertBdd = $bdd->prepare("INSERT INTO xgames(nom, email, objet, message) VALUES (?, ?, ?, ?)");
-        $InsertBdd->execute(array($nom, $email, $objet, $message));
-
-        $reponse = 1;
-        unset ($nom,$email,$objet,$message,$_POST["NOM"],$_POST["EMAIL"],$_POST["OBJET"],$_POST["MESSAGE"]);
-
-    }else{
-        echo "1"; //il faut tout remplir
-    }
-}
-?>
-
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
@@ -32,6 +7,7 @@ if (isset($_POST['NOM']) AND isset($_POST['EMAIL']) AND isset($_POST['OBJET']) A
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>SnowSet</title>
     <link rel="stylesheet" type="text/css" href="css/style_home.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 
 
@@ -42,19 +18,35 @@ if (isset($_POST['NOM']) AND isset($_POST['EMAIL']) AND isset($_POST['OBJET']) A
     <h3>Contact</h3>
 
     <form method="POST">
-        <input type="text" name="NOM" placeholder="Nom">
-        <input type="email" name="EMAIL" placeholder="Adresse email">
-        <input type="text" name="OBJET" placeholder="Objet">
-        <textarea rows="4" name="MESSAGE" placeholder="Message"></textarea>
-        <input type="submit" name="SUBMIT" value="Envoyer">
-        <div id="reponse">
-            <?php if($reponse == 1)
-                echo "Message envoyé";
-            ?>
-        </div>
+        <input type="text" name="name" placeholder="Nom">
+        <input type="email" name="email" placeholder="Adresse email">
+        <input type="text" name="subject" placeholder="Objet">
+        <textarea rows="4" name="message" placeholder="Message"></textarea>
+        <input type="submit" id="submit" value="Envoyer">
+        <div id="reponse"></div>
     </form>
 
 </div>
-
+<script>
+    $("#submit").click(function(e){
+        e.preventDefault();
+        $.post(
+            'includes/config.php',
+            {
+                name : $( "input[name=name]" ).val(),
+                email : $( "input[name=email]" ).val(),
+                subject : $( "input[name=subject]" ).val(),
+                message : $( "textarea[name=message]" ).val()
+            },
+            function NewInfos(data){
+                if(data == "success")
+                    $('#reponse').text("Vous avez bien envoyé votre message.");
+                else
+                    $('#reponse').text(data);
+            }
+        ),
+            'text'
+    });
+</script>
 </body>
 </html>
--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,43 @@
-
 <?php
-session_start();
+
 $bdd = new PDO('mysql:host=localhost;dbname=xgame', 'root', '');
 
+
+function checkForm($name, $email, $subject, $message){
+    if (empty($name))
+        throw new Exception('Veuillez remplir tous les champs.');
+    if (empty($email))
+        throw new Exception('Veuillez remplir tous les champs.');
+    if (empty($subject))
+        throw new Exception('Veuillez remplir tous les champs.');
+    if (empty($message))
+        throw new Exception('Veuillez remplir tous les champs.');
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+        throw new Exception('Email invalide.');
+}
+
+
+if(isset($_POST["name"]) AND isset($_POST["email"]) AND isset($_POST["subject"]) AND isset($_POST["message"])){
+
+    try {
+
+        checkForm($_POST["name"], $_POST["email"], $_POST["subject"], $_POST["message"]);
+
+        $name = htmlspecialchars($_POST['name']);
+        $email = htmlspecialchars($_POST['email']);
+        $subject = htmlspecialchars($_POST['subject']);
+        $message = htmlspecialchars($_POST['message']);
+
+        $InsertBdd = $bdd->prepare("INSERT INTO xgames(name, email, subject, message) VALUES (?, ?, ?, ?)");
+        $InsertBdd->execute(array($name, $email, $subject, $message));
+
+        echo "success";
+
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+
+}
 
 
 ?>
