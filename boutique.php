3ad694b414dd8c4eda65d7b8fe771c7618a51a91
Ahkalamiammiam <vianneytraina@gmail.com>
2018-11-07 15:17:21 +0100

    MAJ boutique.php

---
+++ boutique.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Snowset</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/boutique.css">
+	<link rel="stylesheet" type="text/css" href="css/footer.css">
+	<link rel="stylesheet" type="text/css" href="css/header.css">
+</head>
+
+	<body>
+		<div class="container">
+
+		<?php include 'includes/header.php' ?>
+
+		<main>
+
+			<div class="top-boutique">
+				<article class="boutique-top-left">
+					<img src="img/boutique2019.jpg" alt="">
+				</article>
+
+				<article class="boutique-top-right">
+					<img src="img/boutiquechaussettes.jpg" alt="">
+				</article>
+			</div>
+
+			<div class="bottom-boutique">
+				<article class="boutique-bottom-left">
+					<img src="img/boutiquehomme.jpg" alt="">
+				</article>
+
+				<article class="boutique-bottom-mid">
+					<img src="img/boutiquefemme.jpg" alt="">
+				</article>
+
+				<article class="boutique-bottom-right">
+					<img src="img/boutiqueautre.jpg" alt="">
+				</article>
+			</div>
+
+		</main>
+
+		<?php include 'includes/footer.php' ?>
+
+		</div>
+	</body>
+
+</html>
