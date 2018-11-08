6cbdac1a8212b8f18165c9a4901ab2be0cc301dc
zKolait <nicolas.chapeau@devinci.fr>
2018-11-08 12:33:50 +0100

    update

--- includes/footer.php
+++ includes/footer.php
@@ -4,7 +4,7 @@
 					<li><a href="boutique.php">Boutique</a></li>
 					<li><a href="partenaires.php">Partenaire</a></li>
 					<li><a href="marque.php">Notre marque</a></li>
-					<li><a href="panier.php">Contact</a></li>
+					<li><a href="contact.php">Contact</a></li>
 				</ul>
 			</nav>
 		</footer>
