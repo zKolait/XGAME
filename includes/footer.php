07d4cf35b9bc635ef3ab9f0ba1c31254bc8e3822
zKolait <nicolas.chapeau@devinci.fr>
2018-11-08 09:09:50 +0100

    footer href

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,10 @@
 	    <footer>
 			<nav>
 				<ul>
-					<li><a href="">Boutique</a></li>
-					<li><a href="">Partenaire</a></li>
-					<li><a href="">Notre marque</a></li>
-					<li><a href="">Contact</a></li>
+					<li><a href="boutique.php">Boutique</a></li>
+					<li><a href="partenaires.php">Partenaire</a></li>
+					<li><a href="marque.php">Notre marque</a></li>
+					<li><a href="panier.php">Contact</a></li>
 				</ul>
 			</nav>
 		</footer>
